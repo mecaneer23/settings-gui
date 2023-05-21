d2e5520ec6e5fff96d398bbe4bf60a408a597a2b
mecaneer23 <mecaneer23@gmail.com>
2023-05-21 10:26:49 -0500

    display works, however int values don't update

--- menu.py
+++ menu.py
@@ -37,29 +37,59 @@ class Menu(Gtk.Window):
         self.filename = filename
         self.items = read_file_json(self.filename)
         for index, (key, value) in enumerate(self.items.items()):
-            self.grid.attach(Gtk.Label(label=key), 0, index, 1, 1)
-            self.grid.attach(self.display(key, value), 1, index, 1, 1)
+            self.make_display_box(index, key, value)
 
-    def display_boolean(self, key, value):
+    def make_display_box(self, index, key, value):
+        box = Gtk.Box()
+        box.set_spacing(5)
+        box.pack_start(Gtk.Label(label=key), False, True, 0)
+        box.pack_start(self.display(index, key, value), True, False, 0)
+        self.grid.attach(box, 0, index, 1, 1)
+
+    def display_boolean(self, index, key, value):
         switch = Gtk.Switch()
         switch.set_active(value)
-        switch.connect("notify::active", lambda s, _: self.write_boolean(key, s))
+        switch.connect("notify::active", lambda s, _: self.write_boolean(index, key, s))
         return switch
 
-    def display_int(self, key, value):
-        return Gtk.Entry()
-
-    def display(self, key, value):
-        if isinstance(value, bool):
-            return self.display_boolean(key, value)
-        elif isinstance(value, int):
-            return self.display_int(key, value)
-        else:
-            print(f"Unknown type for {{{key}: {value}}}:", type(value))
-
-    def write_boolean(self, key, switch,):
+    def write_boolean(self, index, key, switch):
         self.items[key] = switch.get_active()
         write_file_json(self.filename, self.items)
+        self.make_display_box(index, key, switch.get_active())
+
+    def display_int(self, index, key, value):
+        box = Gtk.Box()
+        entry = Gtk.Entry()
+        entry.set_text(str(value))
+        box.pack_start(entry, False, True, 0)
+        button_box = Gtk.VBox()
+        up_button = Gtk.Button()
+        up_button.add(
+            Gtk.Arrow(arrow_type=Gtk.ArrowType.UP, shadow_type=Gtk.ShadowType.NONE)
+        )
+        up_button.connect("clicked", lambda _: self.write_int(index, key, value + 1))
+        button_box.pack_start(up_button, True, True, 0)
+        down_button = Gtk.Button()
+        down_button.add(
+            Gtk.Arrow(arrow_type=Gtk.ArrowType.DOWN, shadow_type=Gtk.ShadowType.NONE)
+        )
+        down_button.connect("clicked", lambda _: self.write_int(index, key, value - 1))
+        button_box.pack_start(down_button, True, True, 0)
+        box.pack_start(button_box, True, True, 0)
+        return box
+
+    def write_int(self, index, key, value):
+        self.items[key] = value
+        write_file_json(self.filename, self.items)
+        self.make_display_box(index, key, value)
+
+    def display(self, index, key, value):
+        if isinstance(value, bool):
+            return self.display_boolean(index, key, value)
+        elif isinstance(value, int):
+            return self.display_int(index, key, value)
+        else:
+            print(f"Unknown type for {{{key}: {value}}}:", type(value))
 
 
 def main(filename):
