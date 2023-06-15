44be5ebb097da55ae466f4a53797efb8689362f0
mecaneer23 <mecaneer23@gmail.com>
2023-06-14 23:21:15 -0500

    make booleans work!

--- tkmenu.py
+++ tkmenu.py
@@ -2,18 +2,15 @@
 # pyright: reportMissingImports=false
 
 import customtkinter as ctk
+import json
 
 
 def read_file_json(filename):
-    import json
-
     with open(filename) as f:
         return json.load(f)
 
 
 def write_file_json(filename, contents):
-    import json
-
     with open(filename, "w") as f:
         json.dump(contents, f, indent=4)
 
@@ -26,6 +23,19 @@ def get_args():
     return parser.parse_args()
 
 
+RED = "\033[91m"
+YELLOW = "\033[93m"
+GREEN = "\033[92m"
+CYAN = "\033[96m"
+BLUE = "\033[94m"
+MAGENTA = "\033[95m"
+RESET = "\033[0m"
+
+
+def info(message):
+    print(f"{CYAN}INFO:{RESET} {message}")
+
+
 class Menu(ctk.CTk):
     def __init__(self, filename, filetype="json", **kwargs):
         ctk.CTk.__init__(self, **kwargs)
@@ -33,17 +43,34 @@ class Menu(ctk.CTk):
         self.geometry("400x150")
         self.filename = filename
         self.items = read_file_json(self.filename)
+        self.display_rows = []
         for index, (key, value) in enumerate(self.items.items()):
             self.make_display_box(index, key, value)
 
     def make_display_box(self, index, key, value):
-        ctk.CTkLabel(self, text=key).grid(row=index, column=0)
-        self.display(index, key, value).grid(row=index, column=1, padx=10)
+        self.display_rows.insert(
+            index,
+            [
+                ctk.CTkLabel(self, text=key),
+                self.display(index, key, value),
+            ],
+        )
+        self.display_rows[index][0].grid(row=index, column=0)
+        self.display_rows[index][1].grid(row=index, column=1, padx=10)
 
     def display_boolean(self, index, key, value):
-        switch_value = ctk.StringVar(value=value)
-        switch = ctk.CTkSwitch(self, text=" ", command=lambda: self.write(index, key, bool(int(switch_value.get()))), variable=switch_value)
+        switch = ctk.CTkSwitch(
+            self,
+            text=" ",
+            command=lambda: self.update_bool(index, key, switch),
+        )
+        switch.select() if value else switch.deselect()
         return switch
+
+    def update_bool(self, index, key, switch):
+        item = self.display_rows[index][1]
+        item.select() if switch.get() else item.deselect()
+        self.write(key, bool(switch.get()))
 
     def display_int(self, index, key, value):
         # box = ctk.Box()
@@ -67,10 +94,11 @@ class Menu(ctk.CTk):
         # return box
         return ctk.CTkLabel(self, text="hello world")
 
-    def write(self, index, key, value):
+    def write(self, key, value):
+        original_value = self.items[key]
         self.items[key] = value
         write_file_json(self.filename, self.items)
-        self.make_display_box(index, key, value)
+        info(f"set `{key}` to `{value}` (was `{original_value}`)")
 
     def display(self, index, key, value):
         if isinstance(value, bool):
